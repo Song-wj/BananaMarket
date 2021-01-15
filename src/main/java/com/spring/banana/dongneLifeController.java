ad7dc8ecf708f6f86da40daaf52bbada1bdd05e5
dohee08 <ehgml1996@naver.com>
2021-01-15 15:46:59 +0900

    동네 충돌

--- src/main/java/com/spring/banana/dongneLifeController.java
+++ src/main/java/com/spring/banana/dongneLifeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class dongneLifeController {
 	
-	@RequestMapping(value ="/dongneLife", method = RequestMethod.GET)
+	@RequestMapping(value ="/dongneLife.do", method = RequestMethod.GET)
 	public String dongnelife() {
 	
 		return "/dongneLife/dongneLife";
